205cc0f6cd11bf4cc914feb8e720352b6c2f4d19
Jakub Turek <kubo.bubo@interia.pl>
2022-10-30 20:02:40 +0100

    1/3 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startBtn = document.querySelector("button[data-start]"); 
 
+const stopBtn = document.querySelector("button[data-stop]"); 
+
+const body = document.querySelector("body"); 
+
+let changeColor = null;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+startBtn.addEventListener("click", () => {
+
+startBtn.setAttribute('disabled', ''); 
+
+
+ changeColor = setInterval(() => {
+
+    body.style.backgroundColor = getRandomHexColor()
+}, 1000)
+
+})
+
+stopBtn.addEventListener("click", () => {
+
+    clearInterval(changeColor); 
+    startBtn.removeAttribute("disabled")
+
+})

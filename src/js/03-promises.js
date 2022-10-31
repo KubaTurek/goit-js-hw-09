8655bb66f2f37b5234344240af416acf8becebe3
Jakub Turek <kubo.bubo@interia.pl>
2022-10-31 23:08:34 +0100

    finishing task 3/3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
+const firstDelay = document.querySelector('input[name="delay"]');
+const delayStep = document.querySelector('input[name="step"]');
+const inputAmount = document.querySelector('input[name="amount"]');
+const createPromisesBtn = document.querySelector("button"); 
+const promiseForm = document.querySelector(".form");
+
+
 function createPromise(position, delay) {
+
+  return new Promise((resolve, reject) => {
+setTimeout(() => {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    resolve({position, delay})
   } else {
-    // Reject
+    reject({position, delay})
   }
+}, delay)
+  })
 }
+
+function submitOperator(event) {
+  event.preventDefault();
+
+  const amount = inputAmount.value;
+  const delayStepValue = delayStep.value;
+  setInterval(() => {
+
+    for(let i = 1; i >= amount; i++) {
+createPromise(i, )
+    }
+})
+  }
+
+
+promiseForm.addEventListener("submit", submitOperator )

1cd253cd87ab3d95f7262dc425ea7049e3a5b815
KubaTurek <105357379+KubaTurek@users.noreply.github.com>
2022-11-01 10:48:39 +0000

    Deploying to gh-pages from @ KubaTurek/goit-js-hw-09@010a115317106e4a15663de99d05f2c7960c862b 🚀

--- 03-promises.531fb827.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector('input[name="delay"]');var e=document.querySelector('input[name="step"]'),t=document.querySelector('input[name="amount"]');document.querySelector("button");function n(e,t){return new Promise((function(n,o){setTimeout((function(){Math.random()>.3?n({position:e,delay:t}):o({position:e,delay:t})}),t)}))}document.querySelector(".form").addEventListener("submit",(function(o){o.preventDefault();var u=t.value;e.value,setInterval((function(){for(var e=1;e>=u;e++)n(e)}))}))}();
-//# sourceMappingURL=03-promises.531fb827.js.map
